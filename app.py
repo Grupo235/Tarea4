70a120c680a1ee95a60d90b5f8a4969722abb4bd
DanielPerez <danyferkz@gmail.com>
2026-05-02 01:19:45 -0500

    primer commit

---
+++ app.py
@@ -0,0 +1,13 @@
+""" se debe desarrollar un sistema integral orientado a objetos, sin uso de bases de datos,
+capaz de gestionar clientes, servicios y reservas para una empresa
+llamada Software FJ que ofrece varios tipos de servicios (reservas de
+salas, alquiler de equipos y asesorías especializadas). El objetivo de esta
+tarea es construir una aplicación estable, modular y extensible que
+implemente de forma rigurosa los principios de abstracción, herencia,
+polimorfismo, encapsulación y manejo avanzado de excepciones,
+garantizando que el sistema siga funcionando aun cuando se presenten
+errores durante su ejecución"""
+
+
+
+
