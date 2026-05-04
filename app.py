d78e8c10e8b818ed040e8ed5f77f8c3ed1bc9d48
DanielPerez <danyferkz@gmail.com>
2026-05-03 22:22:40 -0500

    App Base

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-﻿""" INSTRUCCIONES: se debe desarrollar un sistema integral orientado a objetos, sin uso de bases de datos,
+﻿""" INSTRUCCIONES: Se debe desarrollar un sistema integral orientado a objetos, sin uso de bases de datos,
 capaz de gestionar clientes, servicios y reservas para una empresa
-llamada Software FJ que ofrece varios tipos de servicios (reservas de
+llamada (Software FJ) que ofrece varios tipos de servicios (reservas de
 salas, alquiler de equipos y asesorías especializadas). El objetivo de esta
 tarea es construir una aplicación estable, modular y extensible que
 implemente de forma rigurosa los principios de abstracción, herencia,
